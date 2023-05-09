f33f391001e8147d117d92d9212e9b623077a862
joshalbert2433 <joshua.albert.villanueva@gmail.com>
2023-05-09 11:55:41 +0800

    fix redirect page 404

--- client/src/api/Ecomm.api.js
+++ client/src/api/Ecomm.api.js
@@ -1,9 +1,6 @@
 import Axios from "axios";
 
 const Ecomm = Axios.create({
-	// baseURL: "https://ezpc-backend-joshalbert.onrender.com/api/",
-	// baseURL: "http://localhost:8080/api/",
-	// baseURL: "http://localhost:8080/api/",
 	baseURL: process.env.REACT_APP_BASE_URL,
 	headers: {},
 });
--- server/config/database.config.js
+++ server/config/database.config.js
@@ -3,6 +3,6 @@ const mongoose = require("mongoose");
 module.exports = mongoose.connect(process.env.MONGO_URL);
 console.log(process.env.MONGO_URL);
 mongoose.connection.on("connected", () => console.log("Connected"));
-mongoose.connection.on("error", () =>
+mongoose.connection.on("error", (err) =>
 	console.log("Connection failed with - ", err)
 );
